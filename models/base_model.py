02f60c35a74c50bc96c5260198e86991270ef52a
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-08 07:02:16 -0800

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,16 +10,24 @@ class BaseModel:
     """
     BaseModel class
     """
-    def __init__(self, id):
+    def __init__(self, *args, **kwargs):
         """ Initialization """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is not None and kwargs != {}:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ return string representation """
-        return "[{}] ({}) {}".format(self.__class__.__name__,\
-                self.id, self.__dict__)
+        return "[{}] ({}) {}"\
+            .format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """ updates the public instance attribute
