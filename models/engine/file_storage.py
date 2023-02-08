cd97ab50a7439cef970f0a35dd0217166b4128f1
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-08 11:36:43 -0800

    Fix error

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -29,17 +29,16 @@ class FileStorage:
         serializes __objects to the JSON file (path: __file_path)
         """
         with open(self.__file_path, mode="w") as jf:
-            dict_storage = {k: v.obj() for k, v in self.__objects.items()}
+            dict_storage = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(dict_storage, jf)
 
     def reload(self):
         """
         deserializes the JSON file to __objects (only if the JSON file
-        (__file_path) exists; otherwise, do nothing. If the file doesn’t
-        exist, no exception should be raised)
+        (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
         """
         if not os.path.isfile(FileStorage.__file_path):
             return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as jf:
-            obj_dict = json.load(jf)
-            FileStorage.__objects = obj_dict
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+           json.load(f)
