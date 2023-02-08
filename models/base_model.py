9ba60cfef2b280df0d8219e2d91dec4520408fd7
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-08 09:48:30 -0800

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ base_model module
 """
 from datetime import datetime
 from uuid import uuid4
+from models import storage
 
 
 class BaseModel:
@@ -23,6 +24,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """ return string representation """
@@ -33,6 +35,7 @@ class BaseModel:
         """ updates the public instance attribute
         update_at with the current datetime """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/
