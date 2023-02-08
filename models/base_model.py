13af62fcdcc8b3cc014bcca314300b6d62bf232e
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-08 05:24:59 -0800

    BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+base_model module
+"""
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """
+    BaseModel class
+    """
+    def __init__(self, id):
+        """ Initialization """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """ return string representation """
+        return "[{}] ({}) {}".format(self.__class__.__name__,\
+                self.id, self.__dict__)
+
+    def save(self):
+        """ updates the public instance attribute
+        update_at with the current datetime """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """ returns a dictionary containing all keys/
+        values of a __dict__ of the instance """
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = self.__class__.__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
