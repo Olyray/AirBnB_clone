3dd40cc98a8b1d1e5b5f80e478b744ed0aec2e5c
Olyray <46930761+Olyray@users.noreply.github.com>
2023-02-10 18:27:20 +0000

    Fri, Feb 10, 2023, 6:27 PM +00:00

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -2,4 +2,6 @@
 1. Check that it returns id.
 2. Check that it prints the right error when no class is provided
 3. Check that it prints the right error when wrong class is provided
+4. Check do_show
+    a. if 
 """
