a60c59d66c3ac69e6fb973f4b23554d7d607c521
Olamide <olire96@gmail.com>
2023-02-10 21:29:53 +0100

    debug None

--- console.py
+++ console.py
@@ -49,7 +49,41 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class name missing **")
 
+    def do_destroy(self, line):
+        """
+		destroy: Deletes an instance based on the class name and id 
+        (save the change into the JSON file). Ex: $ destroy BaseModel 1234-1234-1234.
+        """
+        the_dict = storage.all()
+        if line:
+            commands = line.split()
+            if commands[0] != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(commands) < 2:
+                print("** instance id missing **")
+            else:
+                try:
+                    del the_dict["{}.{}".format(commands[0], commands[1])]
+                    storage.save()
+                except KeyError:
+                    print("** no instance found **")
+        else:
+            print("** class name missing **")
     
+    def do_all(self, line):
+        """
+		Prints all string representation of all instances based or not on the class name. Ex: $ all BaseModel or $ all.
+
+			The printed result must be a list of strings (like the example below)
+			If the class name doesn’t exist, print ** class doesn't exist ** (ex: $ all MyModel)
+        """
+        if line == "BaseModel" or line is None:
+            all_objs = storage.all()
+            for obj_id in all_objs.keys():
+                obj = all_objs[obj_id]
+                print(obj)
+        else:
+            print("** class doesn't exist **")
 
     def do_EOF(self, line):
         """ handles EOF """
