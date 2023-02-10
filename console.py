3fab9943ee40376fe90c376a1a583442b9af2874
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-09 17:47:37 -0800

    console

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+console module
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand Class
+    """
+
+    prompt = "(hbnb)"
+
+    def do_EOF(self, line):
+        """ handles EOF """
+        return True
+
+    def do_quit(self, line):
+        """
+        Quits command to exit the program
+        """
+        return True
+
+    def emptyline(self):
+        """ empty line """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
