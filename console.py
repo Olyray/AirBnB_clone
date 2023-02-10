473bc92a6174c534570c8fb52186537f2b5b2e37
Olyray <46930761+Olyray@users.noreply.github.com>
2023-02-10 18:36:25 +0000

    Fri, Feb 10, 2023, 6:36 PM +00:00

--- console.py
+++ console.py
@@ -32,19 +32,26 @@ class HBNBCommand(cmd.Cmd):
         """
          Prints the string representation of an instance based on the class name and id.
         """
+        #Call storage.all to access the persistent dict representation
         the_dict = storage.all()
+        #if there is a command after "show"
         if line:
+            #split the line into its constituent parts
             commands = line.split()
+            #Check that the right class is called
             if commands[0] != "BaseModel":
                 print("** class doesn't exist **")
+            #Check for an instance id.
             elif len(commands) < 2:
                 print("** instance id missing **")
             else:
+                #print the string representation if it's available
                 try:
                     print(the_dict["{}.{}".format(commands[0], commands[1])])
                 except KeyError:
                     print("** no instance found **")
         else:
+            #print the appropriate error message
             print("** class name missing **")
 
     def do_EOF(self, line):
