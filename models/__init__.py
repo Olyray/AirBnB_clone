f43dbb50972f0f7b6d57e2c067dabff76669246c
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-11 13:08:36 -0800

    Fix pycodestyle errors

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,9 @@
 """
-to create a unique FileStorage
+To create a unique FileStorage
 instance for your application
 """
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
