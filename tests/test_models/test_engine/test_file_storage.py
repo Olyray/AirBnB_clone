48c75d9fa334b33b047231de92c74501bf5d5f68
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-02-12 02:17:04 -0800

    add test_file_storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" test_file_storage module """
+from unittest import TestCase
+import pycodestyle
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(TestCase):
+    """
+    TestFileStorage class
+    """
+
+    def pep(self):
+        """ test pep """
+        style = pycodestyle.StyleGuide(quiet=True)
+        files = ['models/engine/file_storage',
+                'test/test_models/test_engine/test_file_storage']
+        result = style.check_files(files)
+        self.assertEqual(result.total_errors, 0,
+                "Found code style errors (and warning).")
+
+    def test_module_doc(self):
+        """
+        test module documentation
+        """
+        doc = __import__('models/engine/file_storage').__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_class_doc(self):
+        """
+        test class documentation
+        """
+        doc = TestFileStorage.__doc__
+        self.assertGreater(len(doc), 1)
